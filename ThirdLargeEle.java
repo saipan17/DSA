97d7e17d905b77877c901ded712ed4ba957a2f2b
saipan17 <msaipanmulook@gmail.com>
2024-03-06 10:36:25 +0530

    Find the third large element from an array

---
+++ ThirdLargeEle.java
@@ -0,0 +1,19 @@
+class ThirdLargeEle
+{
+    int thirdLargest(int a[], int n)
+    {
+	    for(int i = 0; i<n; i++){
+	        
+	         for(int j = i+1; j<n; j++){
+	             
+	             if(a[i]>a[j]){
+	                 
+	                 int temp = a[i];
+	                 a[i] = a[j];
+	                 a[j] = temp;
+	             }
+	         }
+	    }
+	    return a[n-3];
+    }
+}
