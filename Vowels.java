b5b9e628848b922f985e2cfc39dab10aa0d110e4
saipan17 <msaipanmulook@gmail.com>
2024-08-22 22:31:01 +0530

    Counting number of vowels from a string

---
+++ Vowels.java
@@ -0,0 +1,16 @@
+class Vowel{
+    String isVowel(char c){
+        // code here
+        
+        if(c == 'a' || c =='e' || c == 'i' || c =='o' || c == 'u' ||
+        
+           c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'
+        )
+        {
+            
+            return "YES";
+        }
+        
+      return "NO";
+    }
+}
